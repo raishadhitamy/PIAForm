7ba8ba257bc360b69c22bbb21e8bb24054b01143
Francis-Dwiputra-A <francis.dwiputra17@gmail.com>
2022-05-27 14:45:47 +0700

    Register to Login

--- PIAOnlineForm/FormRegistration.aspx.vb
+++ PIAOnlineForm/FormRegistration.aspx.vb
@@ -27,5 +27,9 @@ Public Class FormRegistration
 
         Connect.Close()
     End Sub
+
+    Protected Sub ButtonLogin_Click(sender As Object, e As EventArgs) Handles ButtonLogin.Click
+        Response.Redirect("FormLogin.aspx")
+    End Sub
 End Class
 'Hello'
