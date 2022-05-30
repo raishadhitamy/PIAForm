7e78e01280dc555c7dd54162087a30827abd9167
Francis-Dwiputra-A <francis.dwiputra17@gmail.com>
2022-05-30 13:51:55 +0700

    Fix Checked

--- PIAOnlineForm/AgreementForm.aspx.vb
+++ PIAOnlineForm/AgreementForm.aspx.vb
@@ -15,42 +15,51 @@ Public Class AgreementForm
         Dim manager_approval As String = ""
         If manapproved.Checked = True Then
             manager_approval = "Approved"
-        Else
+        ElseIf mannotapproved.Checked = True Then
             manager_approval = "Not Approved"
+        Else
+            manager_approval = ""
         End If
 
         Dim eng_mgr_approval As String = ""
         If engapproved.Checked = True Then
             eng_mgr_approval = "Approved"
-        Else
+        ElseIf engnotapproved.Checked = True Then
             eng_mgr_approval = "Not Approved"
+        Else
+            eng_mgr_approval = ""
         End If
 
         Dim mat_mgr_planner_approval As String = ""
         If matapproved.Checked = True Then
             mat_mgr_planner_approval = "Approved"
-        Else
+        ElseIf matnotapproved.Checked = True Then
             mat_mgr_planner_approval = "Not Approved"
+        Else
+            mat_mgr_planner_approval = ""
         End If
 
         Dim director_approval As String = ""
         If dirapproved.Checked = True Then
             director_approval = "Approved"
-        Else
+        ElseIf dirnotapproved.Checked = True Then
             director_approval = "Not Approved"
+        Else
+            director_approval = ""
         End If
 
         Dim president_director_approval As String = ""
         If preapproved.Checked = True Then
             president_director_approval = "Approved"
-        Else
+        ElseIf prenotapproved.Checked = True Then
             president_director_approval = "Not Approved"
+        Else
+            president_director_approval = ""
         End If
 
         Connect.Open()
 
-        Dim WebRegistration As String = Nothing
-        Dim Command As New SqlCommand("Update WebRegistration set manager_approval = '" & manager_approval & "', eng_mgr_approval = '" & eng_mgr_approval & "', mat_mgr_planner_approval = '" & mat_mgr_planner_approval & "', director_approval = '" & director_approval & "', president_director_approval = '" & president_director_approval & "' where documentid = '" & documentid & "'", Connect)
+        Dim Command As New SqlCommand("Update frompia set Manager_Approval = '" & manager_approval & "', Eng_Mgr_Approval = '" & eng_mgr_approval & "', Mat_Mgr_Planner_Approval = '" & mat_mgr_planner_approval & "', Director_Approval = '" & director_approval & "', President_Director_Approval = '" & president_director_approval & "' where DocumentID = '" & documentid & "'", Connect)
         Dim v = Command.ExecuteNonQuery()
 
         MsgBox("Data updated successfully", MsgBoxStyle.Information, "Message")
